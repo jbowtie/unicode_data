3343eb95b285413554be1a000081ee65c67e6855
John C Barstow <jbowtie@amathaine.com>
2017-07-10 19:57:14 +1200

    Add codecov.io support

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule UnicodeData.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
+     preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
      description: "Exposes Unicode properties needed for layout and analysis.",
      package: package(),
      deps: deps()]
@@ -30,7 +32,8 @@ defmodule UnicodeData.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, "~> 0.14", only: :dev, runtime: false}]
+    [{:ex_doc, "~> 0.14", only: :dev, runtime: false},
+     {:excoveralls, "~> 0.7.1", only: :test}]
   end
 
   defp package do
